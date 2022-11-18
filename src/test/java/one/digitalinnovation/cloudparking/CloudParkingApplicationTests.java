03116a673a4dbb5911f0368db489114def7c2e32
vinicius-ferreiras <vinicius.ferreira@outlook.com.br>
2022-11-18 12:06:10 -0300

    first commit

---
+++ src/test/java/one/digitalinnovation/cloudparking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.cloudparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
